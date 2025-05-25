82f087ed653fb60ed533cbfd87356f5083fa8444
AlexisLS05 <151126331+AlexisLS05@users.noreply.github.com>
2025-05-26 01:31:23 +0200

    photos

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,14 @@
 window.addEventListener("load", () => {
     const loader = document.getElementById("loader");
-    if (loader) loader.style.display = "none";
+    if (loader) {
+        const delay = Math.random() * 2000 + 1000;
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, delay);
+    }
 });
+
 
 window.addEventListener("beforeunload", () => {
     const loader = document.getElementById("loader");
