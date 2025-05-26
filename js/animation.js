cdafc9e58d9a6fd94d7e02bbfadd3ecdff09f410
AlexisLS05 <151126331+AlexisLS05@users.noreply.github.com>
2025-05-26 18:08:31 +0200

    last changes folow up 3

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
     const loader = document.getElementById("loader");
     if (loader) {
-        const delay = Math.random() * 2000 + 1000;
+        const delay = Math.random() * 1300 + 400;
 
         setTimeout(() => {
             loader.style.display = "none";
